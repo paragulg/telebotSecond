b94c39ecfb82e745e1903b30a4e7c63dffd74a9d
paragulgov06 <77095398+paragulgov06@users.noreply.github.com>
2022-04-27 19:48:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
 import telebot
 import config
-from translate import Translator
+from googletrans import Translator
 
 bot = telebot.TeleBot(config.TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = telebot.types.KeyboardButton('Русский🇷🇺')
+    btn2 = telebot.types.KeyboardButton('Английский🇬🇧')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, text='Привет, {0.first_name}!\nВыбери язык, на который нужен перевод текста: '.format(message.from_user), reply_markup=markup)
+
 @bot.message_handler(content_types='text')
 def translate(message):
-    text = message.text
-    languages = Translator(from_lang='ru', to_lang='en')
-    translatedText = languages.translate(text)
+    if(message.text == 'Русский🇷🇺'):
+        def trans(message):
+            translator = Translator()
+            translateText = translator.translate(message.text, dest='ru')
+            bot.send_message(message.chat.id, text=translateText.text)
+            msg = bot.send_message(message.chat.id, text='Введите текст:')
+            bot.register_next_step_handler(msg, trans)
+        msg = bot.send_message(message.chat.id, text='Введите текст:')
+        bot.register_next_step_handler(msg, trans)
+    elif(message.text =='Английский🇬🇧'):
+        def trans(message):
+            translator = Translator()
+            translateText = translator.translate(message.text, dest='en')
+            bot.send_message(message.chat.id, text=translateText.text)
+            msg = bot.send_message(message.chat.id, text='Введите текст:')
+            bot.register_next_step_handler(msg, trans)
+        msg = bot.send_message(message.chat.id, text='Введите текст:')
+        bot.register_next_step_handler(msg, trans)
 
-    bot.send_message(message.chat.id, translatedText)
-    
 bot.polling(none_stop=True)
