bf5a8628bfc61c77d790798c3650fdd1a2f115eb
paragulgov06 <77095398+paragulgov06@users.noreply.github.com>
2022-05-22 13:13:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from googletrans import Translator
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -14,38 +15,27 @@ def start(message):
     markup.add(btn1, btn2, btn3, btn4)
     bot.send_message(message.chat.id, text='Привет, {0.first_name}!\nВыбери язык, на который нужен перевод текста: '.format(message.from_user), reply_markup=markup)
 
+def func(message, lang):
+    def trans(message):
+        translator = Translator()
+        translateText = translator.translate(message.text, dest=lang)
+        bot.send_message(message.chat.id, text=translateText.text)
+    msg = bot.send_message(message.chat.id, text='Введите текст:')
+    bot.register_next_step_handler(msg, trans)
+
+
 @bot.message_handler(content_types='text')
 def translate(message):
     try:
         if(message.text == 'Русский🇷🇺'):
-            def trans(message):
-                translator = Translator()
-                translateText = translator.translate(message.text, dest='ru')
-                bot.send_message(message.chat.id, text=translateText.text)
-            msg = bot.send_message(message.chat.id, text='Введите текст:')
-            bot.register_next_step_handler(msg, trans)
-        elif(message.text =='Английский🇬🇧'):
-            def trans(message):
-                translator = Translator()
-                translateText = translator.translate(message.text, dest='en')
-                bot.send_message(message.chat.id, text=translateText.text)
-            msg = bot.send_message(message.chat.id, text='Введите текст:')
-            bot.register_next_step_handler(msg, trans)
+            func(message, 'ru')
+        elif(message.text == 'Английский🇬🇧'):
+            func(message, 'en')
         elif(message.text == 'Арабский🇸🇦'):
-            def trans(message):
-                translator = Translator()
-                translateText = translator.translate(message.text, dest='ar')
-                bot.send_message(message.chat.id, text=translateText.text)
-            msg = bot.send_message(message.chat.id, text='Введите текст:')
-            bot.register_next_step_handler(msg, trans)
-        elif(message.text =='Итальянский🇮🇹'):
-            def trans(message):
-                translator = Translator()
-                translateText = translator.translate(message.text, dest='it')
-                bot.send_message(message.chat.id, text=translateText.text)
-            msg = bot.send_message(message.chat.id, text='Введите текст:')
-            bot.register_next_step_handler(msg, trans)
+            func(message, 'ar')
+        elif(message.text == 'Итальянский🇮🇹'):
+            func(message, 'it')
     except Exception:
-        bot.send_message(message.chat.id, text='Упс... Произошла ошибка, перепроверьте написанный текст.')
+        bot.send_message(message.chat.id, text='Упс... Произошла ошибка!')
 
 bot.polling(none_stop=True)
